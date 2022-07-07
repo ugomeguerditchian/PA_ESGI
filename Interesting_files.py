fdb0bc4235492102676a61efbc74f4f2cf3f9df8
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-07-07 18:53:43 +0200

    Update Interesting_files.py

--- Interesting_files.py
+++ Interesting_files.py
@@ -1,12 +1,13 @@
 # importing required modules
 import PyPDF2
 import os
-
+from threading import Thread
 # Dictionary
 import docx
 
 extensions = [".txt", ".png", ".docx", ".pdf"]
 bank_data = ["BIC", "Identité", "Bancaire", "toto", "Exemple", "fichier"]
+
 
 def count_interisting_word(words_list):
     count = 0
@@ -15,6 +16,7 @@ def count_interisting_word(words_list):
             if w == word:
                 count += 1
     return count
+
 
 def file_collection(path):
     files_return = []
@@ -47,6 +49,7 @@ def sort_by_priority(tmp_list):
 def search_interisting_files(files_list):
     tmp_list = []
     for file in files_list:
+        print(file)
         if file.endswith('.pdf'):
             tmp_list.append(pdf_parser(file))
         elif file.endswith('.txt'):
@@ -62,62 +65,75 @@ def search_interisting_files(files_list):
 
 
 def pdf_parser(file):
-    count = 0
-    # Ouverture du pdf
-    pdfFileObj = open(file, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    # Si le pdf est chiffré on le flag -1 (high priority)
-    if pdfReader.isEncrypted:
-        count = -1
-        return [file, count]
-    else:
-        # Pour chaque page du pdf
-        for _ in range(0, pdfReader.numPages):
-            pageObj = pdfReader.getPage(0)
-            # On extract le text
-            var = pageObj.extractText()
-            # On le met sous forme de tableau de mot
-            words_list = var.split(" ")
-            # On vérifie le nombre de mot intéressant
-            count += count_interisting_word(words_list)
-        pdfFileObj.close()
-        return [file, count]
+    try:
+        count = 0
+        # Ouverture du pdf
+        pdfFileObj = open(file, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        # Si le pdf est chiffré on le flag -1 (high priority)
+        if pdfReader.isEncrypted:
+            count = -1
+            return [file, count]
+        else:
+            # Pour chaque page du pdf
+            for _ in range(0, pdfReader.numPages):
+                pageObj = pdfReader.getPage(0)
+                # On extract le text
+                var = pageObj.extractText()
+                # On le met sous forme de tableau de mot
+                words_list = var.split(" ")
+                # On vérifie le nombre de mot intéressant
+                count += count_interisting_word(words_list)
+            pdfFileObj.close()
+            return [file, count]
+    except:
+        return [file,0]
 
 
 def docx_parser(file):
-    count = 0
-    doc = docx.Document(file)
-    fullText = []
-    # on recup les paragraphe
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    # on recup les lignes
-    for line in fullText:
-        words_list = line.split(" ")
-        count += count_interisting_word(words_list)
-    return [file, count]
+    try:
+        count = 0
+        doc = docx.Document(file)
+        fullText = []
+        # on recup les paragraphe
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        # on recup les lignes
+        for line in fullText:
+            words_list = line.split(" ")
+            count += count_interisting_word(words_list)
+        return [file, count]
+    except:
+        return [file,0]
 
 
 def txt_parser(file):
-    #On récupère les lignes du fichiers
-    file_ = open(file, mode='r')
-    lines = file_.readlines()
-    file_.close()
+    # On récupère les lignes du fichiers
+    try:
+        file_ = open(file, mode='r')
+        lines = file_.readlines()
+        file_.close()
+        words_list = []
+        for line in lines:
+            for word in line.split(' '):
+                words_list.append(word)
+        # On compte le nombre de mots intéressant dans le fichier
+        count = count_interisting_word(words_list)
+        return [file, count]
+    except:
+        print("le fichier est inouvrable")
+        return [file,0]
     # On les transforme en liste de mots
-    words_list = []
-    for line in lines:
-        for word in line.split(' '):
-            words_list.append(word)
-    # On compte le nombre de mots intéressant dans le fichier
-    count = count_interisting_word(words_list)
-    return [file, count]
 
 
 def main():
     files_list = search_interisting_files(file_collection('testdir'))
-    print(files_list)
+    #print(files_list)
+    return files_list
 
-
+""""
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
     main()
+"""
+#main()
